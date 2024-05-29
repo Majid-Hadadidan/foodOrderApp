8560255cb1697abba8377fce74147e2c258c451f
majidH <majid.hadadian@gmail.com>
2024-05-29 17:26:14 +0330

    add Error component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,18 +1,20 @@
 import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
-
+import Error from "./Error";
 const requestConfig = {};
 export default function Meals() {
-
   //fetch with useHttp
-  const { data: loadingMeals, isLoading } = useHttp(
-    "http://localhost:3000/meals",
-    requestConfig,
-    []
-  );
+  const {
+    data: loadingMeals,
+    error,
+    isLoading,
+  } = useHttp("http://localhost:3000/mealssss", requestConfig, []);
 
   if (isLoading) {
     return <p className="center"> Fetch isLoading...</p>;
+  }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
   }
 
   return (
